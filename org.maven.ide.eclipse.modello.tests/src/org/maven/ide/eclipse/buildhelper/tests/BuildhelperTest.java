e1192af131e24c8b01ebecdede304100047efedd
Igor Fedorenko <igor@ifedorenko.com>
2010-08-25 07:01:49 -0400

    buildhelperConfigurator integration test
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

---
+++ org.maven.ide.eclipse.modello.tests/src/org/maven/ide/eclipse/buildhelper/tests/BuildhelperTest.java
@@ -0,0 +1,33 @@
+package org.maven.ide.eclipse.buildhelper.tests;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
+import org.maven.ide.eclipse.project.ResolverConfiguration;
+import org.maven.ide.eclipse.tests.common.AbstractMavenProjectTestCase;
+
+public class BuildhelperTest
+    extends AbstractMavenProjectTestCase
+{
+    public void test_p001_simple()
+        throws Exception
+    {
+        ResolverConfiguration configuration = new ResolverConfiguration();
+        IProject project1 = importProject( "projects/buildhelper/buildhelper-001/pom.xml", configuration );
+        waitForJobsToComplete();
+
+        project1.build( IncrementalProjectBuilder.FULL_BUILD, monitor );
+        waitForJobsToComplete();
+
+        assertNoErrors( project1 );
+
+        IJavaProject javaProject1 = JavaCore.create( project1 );
+        IClasspathEntry[] cp1 = javaProject1.getRawClasspath();
+
+        assertEquals( new Path( "/buildhelper-001/src/custom/java" ), cp1[3].getPath() );
+    }
+
+}
