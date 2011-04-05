73ccd6043507cd9159abba52dd701b1b9058b8b5
vladt <vladt@sonatype.com>
2011-04-05 17:21:47 -0400

    336521 adopting IPluginExecutionMetadata changes in m2e-core

--- org.sonatype.m2e.antlr/src/org/sonatype/m2e/antlr/internal/AntlrProjectConfigurator.java
+++ org.sonatype.m2e.antlr/src/org/sonatype/m2e/antlr/internal/AntlrProjectConfigurator.java
@@ -9,7 +9,7 @@
 package org.sonatype.m2e.antlr.internal;
 
 import org.apache.maven.plugin.MojoExecution;
-import org.eclipse.m2e.core.internal.lifecyclemapping.model.PluginExecutionMetadata;
+import org.eclipse.m2e.core.lifecyclemapping.model.IPluginExecutionMetadata;
 import org.eclipse.m2e.core.project.IMavenProjectFacade;
 import org.eclipse.m2e.core.project.configurator.AbstractBuildParticipant;
 import org.sonatype.m2e.mojos.internal.AbstractJavaProjectConfigurator;
@@ -17,9 +17,10 @@ import org.sonatype.m2e.mojos.internal.AbstractJavaProjectConfigurator;
 public class AntlrProjectConfigurator
     extends AbstractJavaProjectConfigurator
 {
+    @Override
     public AbstractBuildParticipant getBuildParticipant( IMavenProjectFacade projectFacade,
                                                          MojoExecution execution,
-                                                         PluginExecutionMetadata executionMetadata )
+                                                         IPluginExecutionMetadata executionMetadata )
     {
         return new AntlrBuildParticipant( execution );
     }
