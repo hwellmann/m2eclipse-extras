0170d1f643670325acf5248a920b9f38f4d60d05
Fred Bricon <fbricon@gmail.com>
2011-07-04 17:33:02 +0200

    Rename the mavenarchiver.pomproperties plugin to mavenarchiver
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- org.sonatype.m2e.mavenarchiver/src/org/sonatype/m2e/mavenarchiver/internal/PomPropertiesConfigurator.java
+++ org.sonatype.m2e.mavenarchiver/src/org/sonatype/m2e/mavenarchiver/internal/PomPropertiesConfigurator.java
@@ -6,7 +6,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
 
-package org.sonatype.m2e.mavenarchiver.pomproperties.internal;
+package org.sonatype.m2e.mavenarchiver.internal;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
