00e550578f7abd7879d8f00adabfe7c6b24fafa1
Harald Wellmann <harald.wellmann@gmx.de>
2011-07-14 23:45:07 +0200

    m2e extension for OpenJPA enhancement

---
+++ org.omadac.m2e.openjpa/src/org/omadac/m2e/openjpa/internal/OpenJpaProjectConfigurator.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Harald Wellmann
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.omadac.m2e.openjpa.internal;
+
+import java.io.File;
+
+import org.apache.maven.plugin.MojoExecution;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.m2e.core.lifecyclemapping.model.IPluginExecutionMetadata;
+import org.eclipse.m2e.core.project.IMavenProjectFacade;
+import org.eclipse.m2e.core.project.configurator.AbstractBuildParticipant;
+import org.eclipse.m2e.core.project.configurator.ProjectConfigurationRequest;
+import org.eclipse.m2e.jdt.AbstractJavaProjectConfigurator;
+
+public class OpenJpaProjectConfigurator extends AbstractJavaProjectConfigurator {
+
+    @Override
+    public AbstractBuildParticipant getBuildParticipant(IMavenProjectFacade projectFacade,
+            MojoExecution execution, IPluginExecutionMetadata executionMetadata) {
+        return new OpenJpaBuildParticipant(execution);
+    }
+
+    /**
+     * The openjpa-maven-plugin does not add any source folders.
+     */
+    protected File[] getSourceFolders(ProjectConfigurationRequest request,
+            MojoExecution mojoExecution) throws CoreException {
+        return new File[0];
+    }
+}
