d231f20ed883880a92f8a73987879c651fe92ed9
Phil Webb <phil.webb@orbweaver.co.uk>
2011-09-13 14:38:34 +0100

    Tests for build helper resources

--- org.sonatype.m2e.buildhelper.tests/projects/buildhelper/buildhelper-002/src/custom/test/java/buildhelper002/custom/CustomTreeClassTest.java
+++ org.sonatype.m2e.buildhelper.tests/projects/buildhelper/buildhelper-002/src/custom/test/java/buildhelper002/custom/CustomTreeClassTest.java
@@ -3,16 +3,15 @@ package buildhelper002.custom;
 import java.io.InputStream;
 
 import junit.framework.TestCase;
-
-import org.apache.commons.io.IOUtils;
-
 import buildhelper002.main.MainTreeClass;
 
 public class CustomTreeClassTest extends TestCase {
 
 	public void testRead() throws Exception {
-		InputStream in = getClass().getResourceAsStream("mainTreeTest.txt");
-		String expected = IOUtils.toString(in);
+		InputStream in = getClass().getResourceAsStream("customTreeTest.txt");
+    	byte[] bytes =new byte[6];
+    	in.read(bytes);
+    	String expected = new String(bytes);
 		String actual = new MainTreeClass().read();
 		assertEquals(expected, actual);
 	}
--- org.sonatype.m2e.buildhelper.tests/src/org/sonatype/m2e/buildhelper/tests/BuildhelperTest.java
+++ org.sonatype.m2e.buildhelper.tests/src/org/sonatype/m2e/buildhelper/tests/BuildhelperTest.java
@@ -1,5 +1,7 @@
 package org.sonatype.m2e.buildhelper.tests;
 
+import java.io.File;
+
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.runtime.Path;
@@ -9,25 +11,50 @@ import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.m2e.core.project.ResolverConfiguration;
 import org.eclipse.m2e.tests.common.AbstractMavenProjectTestCase;
 
+@SuppressWarnings( "restriction" )
 public class BuildhelperTest
     extends AbstractMavenProjectTestCase
 {
     public void test_p001_simple()
         throws Exception
     {
-        ResolverConfiguration configuration = new ResolverConfiguration();
-        IProject project1 = importProject( "projects/buildhelper/buildhelper-001/pom.xml", configuration );
-        waitForJobsToComplete();
-
-        project1.build( IncrementalProjectBuilder.FULL_BUILD, monitor );
-        waitForJobsToComplete();
-
-        assertNoErrors( project1 );
-
-        IJavaProject javaProject1 = JavaCore.create( project1 );
-        IClasspathEntry[] cp1 = javaProject1.getRawClasspath();
-
-        assertEquals( new Path( "/buildhelper-001/src/custom/java" ), cp1[3].getPath() );
+        IProject project = importBuildHelperProject( "buildhelper-001" );
+        IJavaProject javaProject = JavaCore.create( project );
+        IClasspathEntry[] classpath = javaProject.getRawClasspath();
+        assertEquals( new Path( "/buildhelper-001/src/custom/java" ), classpath[3].getPath() );
     }
 
+    public void test_p002_resources()
+        throws Exception
+    {
+        IProject project = importBuildHelperProject( "buildhelper-002" );
+        IJavaProject javaProject = JavaCore.create( project );
+        IClasspathEntry[] classpath = javaProject.getRawClasspath();
+        assertEquals( new Path( "/buildhelper-002/src/custom/main/resources" ), classpath[4].getPath() );
+        assertEquals( new Path( "/buildhelper-002/src/custom/main/java" ), classpath[5].getPath() );
+        assertEquals( new Path( "/buildhelper-002/src/custom/test/java" ), classpath[6].getPath() );
+        assertEquals( new Path( "/buildhelper-002/src/custom/test/resources" ), classpath[7].getPath() );
+
+        File target = new File( "projects/buildhelper/buildhelper-002/target" );
+        assertTrue( "Class", new File( target, "classes/buildhelper002/custom/CustomTreeClass.class" ).exists() );
+        assertTrue( "Resource", new File( target, "classes/buildhelper002/custom/CustomTree.txt" ).exists() );
+        assertTrue( "Test Class",
+                    new File( target, "test-classes/buildhelper002/custom/CustomTreeClassTest.class" ).exists() );
+        assertTrue( "Test Resource",
+                    new File( target, "test-classes/buildhelper002/custom/customTreeTest.txt" ).exists() );
+    }
+
+    private IProject importBuildHelperProject( String name )
+        throws Exception
+    {
+        ResolverConfiguration configuration = new ResolverConfiguration();
+        IProject project = importProject( "projects/buildhelper/" + name + "/pom.xml", configuration );
+        waitForJobsToComplete();
+
+        project.build( IncrementalProjectBuilder.FULL_BUILD, monitor );
+        waitForJobsToComplete();
+
+        assertNoErrors( project );
+        return project;
+    }
 }
