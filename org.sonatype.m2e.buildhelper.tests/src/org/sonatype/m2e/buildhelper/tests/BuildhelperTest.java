57cf539c87e8b69a1b1a22804a215833fc607459
Igor Fedorenko <igor@ifedorenko.com>
2011-09-28 23:45:54 -0400

    349586 reworked buildhelper support using m2e 1.1 API
    also updated version to 0.15 to signify dependency on m2e 1.1
    
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

--- org.sonatype.m2e.buildhelper.tests/src/org/sonatype/m2e/buildhelper/tests/BuildhelperTest.java
+++ org.sonatype.m2e.buildhelper.tests/src/org/sonatype/m2e/buildhelper/tests/BuildhelperTest.java
@@ -10,7 +10,6 @@ import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.m2e.core.project.ResolverConfiguration;
 import org.eclipse.m2e.tests.common.AbstractMavenProjectTestCase;
-import org.junit.Ignore;
 
 @SuppressWarnings( "restriction" )
 public class BuildhelperTest
@@ -22,7 +21,13 @@ public class BuildhelperTest
         IProject project = importBuildHelperProject( "buildhelper-001" );
         IJavaProject javaProject = JavaCore.create( project );
         IClasspathEntry[] classpath = javaProject.getRawClasspath();
-        assertEquals( new Path( "/buildhelper-001/src/custom/java" ), classpath[3].getPath() );
+
+        assertEquals( 4, classpath.length );
+
+        assertEquals( new Path( "/buildhelper-001/src/main/java" ), classpath[0].getPath() );
+        assertEquals( new Path( "/buildhelper-001/src/custom/java" ), classpath[1].getPath() );
+        assertEquals( "org.eclipse.jdt.launching.JRE_CONTAINER", classpath[2].getPath().segment( 0 ) );
+        assertEquals( new Path( "org.eclipse.m2e.MAVEN2_CLASSPATH_CONTAINER" ), classpath[3].getPath() );
     }
 
     public void test_p002_resources()
@@ -31,13 +36,19 @@ public class BuildhelperTest
         IProject project = importBuildHelperProject( "buildhelper-002" );
         IJavaProject javaProject = JavaCore.create( project );
         IClasspathEntry[] classpath = javaProject.getRawClasspath();
-        assertEquals( new Path( "/buildhelper-002/src/custom/main/resources" ), classpath[4].getPath() );
-        assertEquals( new Path( "/buildhelper-002/src/custom/main/java" ), classpath[5].getPath() );
-        assertEquals( new Path( "/buildhelper-002/src/custom/test/java" ), classpath[6].getPath() );
-        assertEquals( new Path( "/buildhelper-002/src/custom/test/resources" ), classpath[7].getPath() );
-        
+
+        assertEquals( 8, classpath.length );
+        assertEquals( new Path( "/buildhelper-002/src/main/java" ), classpath[0].getPath() );
+        assertEquals( new Path( "/buildhelper-002/src/custom/main/java" ), classpath[1].getPath() );
+        assertEquals( new Path( "/buildhelper-002/src/main/resources" ), classpath[2].getPath() );
+        assertEquals( new Path( "/buildhelper-002/src/custom/main/resources" ), classpath[3].getPath() );
+        assertEquals( new Path( "/buildhelper-002/src/custom/test/java" ), classpath[4].getPath() );
+        assertEquals( new Path( "/buildhelper-002/src/custom/test/resources" ), classpath[5].getPath() );
+        assertEquals( "org.eclipse.jdt.launching.JRE_CONTAINER", classpath[6].getPath().segment( 0 ) );
+        assertEquals( new Path( "org.eclipse.m2e.MAVEN2_CLASSPATH_CONTAINER" ), classpath[7].getPath() );
+
         File target = project.findMember( "target" ).getRawLocation().toFile();
-        assertTrue( target+" does not exist", target.exists() );
+        assertTrue( target + " does not exist", target.exists() );
         assertTrue( "Class", new File( target, "classes/buildhelper002/custom/CustomTreeClass.class" ).exists() );
         assertTrue( "Resource", new File( target, "classes/buildhelper002/custom/customTree.txt" ).exists() );
         assertTrue( "Test Class",
